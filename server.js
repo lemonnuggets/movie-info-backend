1f62669b8042f47203a5d548c61f34e4929c12c4
lemonnuggets <63046776+lemonnuggets@users.noreply.github.com>
2021-03-02 22:14:10 +0530

    Changed formatting.

--- server.js
+++ server.js
@@ -1,32 +1,49 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
-const fetch = require("node-fetch")
+const fetch = require("node-fetch");
 const app = express();
+
 let port = process.env.PORT || 3000;
-app.set("view engine", "ejs")
-app.use(express.static("public"))
-async function getInfo(movieName, year){
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+async function getInfo(movieName, year) {
     const API_KEY = process.env.API_KEY;
+    // prettier-ignore
     let url = `https://www.omdbapi.com/?t=${movieName}${year!=''?('&y='+year):''}&apikey=${API_KEY}`
-    console.log(url)
-    let response = await fetch(url)
-    let data = await response.json()
-    return data
+    console.log(url);
+    let response = await fetch(url);
+    let data = await response.json();
+    return data;
 }
+
 app.listen(port, () => {
-    console.log(`Listening at ${port}`)
-})
-app.get('/', (req, res)=>{
-    res.sendFile('./views/index.html', {root: __dirname})
+    console.log(`Listening at ${port}`);
 });
-app.get('/request', (req, res)=> {
-    movieData = getInfo(req.query.name, req.query.year)
-    movieData.then(result => {
-        const miniResult = {Title: result.Title, Plot:result.Plot, Poster: result.Poster, Year: result.Year}
-        res.render('result', {data: miniResult, name: req.query.name, year: req.query.year})
-        // console.log(result)
-    }).catch((err) => {
-        console.err(err)
-        res.sendFile('./views/index.html', {root: __dirname})
-    })
-})
+
+app.get("/", (req, res) => {
+    res.sendFile("./views/index.html", { root: __dirname });
+});
+
+app.get("/request", (req, res) => {
+    movieData = getInfo(req.query.name, req.query.year);
+    movieData
+        .then((result) => {
+            const miniResult = {
+                Title: result.Title,
+                Plot: result.Plot,
+                Poster: result.Poster,
+                Year: result.Year,
+            };
+            res.render("result", {
+                data: miniResult,
+                name: req.query.name,
+                year: req.query.year,
+            });
+            // console.log(result)
+        })
+        .catch((err) => {
+            console.err(err);
+            res.sendFile("./views/index.html", { root: __dirname });
+        });
+});
